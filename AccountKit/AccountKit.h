ac9d06ca9c0c1ebdc383ef7b9ecb48b3b26303cb
Hoju Choi <5stralia@gmail.com>
2023-04-09 20:56:30 +0900

    Initial Commit

---
+++ AccountKit/AccountKit.h
@@ -0,0 +1,18 @@
+//
+//  AccountKit.h
+//  AccountKit
+//
+//  Created by Hoju Choi on 2023/04/09.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AccountKit.
+FOUNDATION_EXPORT double AccountKitVersionNumber;
+
+//! Project version string for AccountKit.
+FOUNDATION_EXPORT const unsigned char AccountKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccountKit/PublicHeader.h>
+
+
